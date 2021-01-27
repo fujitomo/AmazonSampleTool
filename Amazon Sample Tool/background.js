51da8b7c0e6183b5fb2f711a96f2982d4accddc9
FUJITOMO375 <fujitomo375@gmail.com>
2021-01-27 23:32:18 +0900

    ひとまず完成

---
+++ Amazon Sample Tool/background.js
@@ -0,0 +1,32 @@
+﻿//Amazon検索ボックスに入れる値
+let send_val = undefined
+
+//chrome.browserAction.onClicked.addListener(function(tab) {
+    //タブを作成
+//    chrome.tabs.create({
+//        url: "https://www.amazon.co.jp"
+//    });
+//});
+
+//メッセージリスナー
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+    // ページ読み込み時
+    if (request.name === "send_load" && typeof send_val !== "undefined") {
+        //alert("send_load:" + send_val);
+        let response = {send_val: send_val,name:"send_background"};
+        sendResponse(response);
+        //残るのでクリア
+        send_val = undefined
+    }
+
+    // clickイベント時
+    if (request.name === "send_click") {
+          //alert("send_click:" + request.send_val);
+          send_val = request.send_val;
+        //タブを作成
+        chrome.tabs.create({
+            url: "https://www.amazon.co.jp"
+        });
+    }
+});
---
+++ Amazon Sample Tool/content.js
@@ -0,0 +1,18 @@
+﻿
+
+//メッセージ送信
+chrome.runtime.sendMessage({name: "send_load" ,send_val: ""}, function(response) {
+	//alert("コンテントsendMessage:" + response.send_val);
+	if(response.name==="send_background") {
+		//alert(response.name);
+		document.getElementById("twotabsearchtextbox").value = response.send_val
+	}
+});
+
+document.getElementById("search").addEventListener( 'click', onClick, true ) ;
+function onClick( e ) {
+	 let send_val = document.getElementById("search_value").value
+	 //alert(send_val);
+	 if (!send_val){ return false;}
+     chrome.runtime.sendMessage({name: "send_click",send_val: send_val});
+}
